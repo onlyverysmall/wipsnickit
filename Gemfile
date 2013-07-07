763a1e5f94a840afe1f6533a5dcae3b0b3b7cad3
Sarah Hadley <sarah@onlyverysmall.com>
2013-07-06 22:22:04 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg'
+gem 'backbone-on-rails'
+gem 'bcrypt-ruby', '~> 3.0.0', require: "bcrypt"
+gem 'jquery-rails'
+gem 'underscore-rails'
+
+group :development do
+  gem 'annotate'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wipsnickit::Application.config.secret_token = '9123d889b8b3548b38d0aaf8c95739e45ec331477e68995bc5ebb71fe8049e87c92877909de98223c26633e7ff68ee0e37bcfba0f4797984ad1d7c8e74a9c113'
